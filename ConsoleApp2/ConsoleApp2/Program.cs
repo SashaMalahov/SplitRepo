622caf8f6fc767aac25c0093c7db5dbb488167b2
LAPTOP-H46IQ2UJ\Aliaksandr <aliaksandr.malakhau@winfinity.live>
2023-08-24 17:53:40 +0300

    test commit

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Commit to add some history changes.");
