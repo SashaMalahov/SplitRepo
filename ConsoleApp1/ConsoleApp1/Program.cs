c6891a172d2d8a0614c05b7ea7c31193a404ec62
LAPTOP-H46IQ2UJ\Aliaksandr <aliaksandr.malakhau@winfinity.live>
2023-08-24 17:55:49 +0300

    One more commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Test commit to change history");
 Console.WriteLine("One more commit to change history");
+Console.WriteLine("And one more commit to change history");
